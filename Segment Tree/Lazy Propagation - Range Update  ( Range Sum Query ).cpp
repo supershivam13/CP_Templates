73560929e1f06607d887e2375b42324102c688f8
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-06-15 14:22:39 +0530

    Create Lazy Propagation - Range Update  ( Range Sum Query ).cpp

---
+++ Segment Tree/Lazy Propagation - Range Update  ( Range Sum Query ).cpp
@@ -0,0 +1,268 @@
+/*
+    Author      : SHIVAM SAHU
+    Institution : IIT PATNA
+    Country     : INDIA
+*/
+
+#include<bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// PRAGMA reduces execution time a little
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+
+// The typedef is used to give data type a new name
+// #define in C is a directive which is used to #define alias(shortcuts)
+typedef long long ll;
+typedef long double ld;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+#define f(i,n)          for(int i=0;i<n;i++)
+#define F(i,a,n)        for(int i=a;i<n;i++)
+#define fo(i,a,b)       for(int i=a;i<=b;i++)
+#define arrin(a,n)      for(int i=0;i<n;i++)cin>>a[i]
+#define arrout(a,n)     for(int i=0;i<n;i++)cout<<a[i]<<" "
+#define print2d(a,n,m)  for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<"\n";}
+#define w(x)            int x; cin>>x; while(x--)
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define double          long double
+#define eb              emplace_back
+#define pb              push_back
+#define pf              push_front
+#define popb            pop_back
+#define popf            pop_front
+#define mp              make_pair
+#define rt              return
+#define br              break
+#define ct              continue
+#define elif            else if
+
+#define dbg             cout<<"\nhuehue\n"
+#define sayy            cout<<"YES\n"
+#define sayn            cout<<"NO\n"
+#define debug(a)        cerr << #a << ": " << a << endl
+
+
+#define pii             pair<int,int>  // ( p.first , p.second )
+#define pss             pair<string,string>
+
+#define mii             map<int,int> // ( insert(key,value), begin(), end(), size(), empty() )
+#define mci             map<char, int>
+#define msi             map<string, int>
+#define mss             map<string, string>
+#define umii            unordered_map<int,int>
+// Itertating over a map m ==>> for( auto i : m ){ cout<<i.first<<" "<<i.second<<endl; }
+
+#define vi              vector<int>  // ( size(), resize(), begin(), end(), empty() )
+#define vb              vector<bool>
+#define vii             vector<pair<int,int>>
+#define vvi             vector<vector<int>>
+
+#define si              set<int>  // stores in ascending order // ( insert(), size(), empty(), begin(), end() )
+#define sid             set<int, greater<int> >  // stores in descending order
+#define sii             set<pair<int,int>>
+
+#define li              list <int> // ( pb(), pf(), popb(), popf(), reverse(), sort(), empty(), size(),
+//  remove(), front(), back() insert() )
+
+#define maxheap         priority_queue<int>  // by default, max heap ( push(), pop(), top(), empty(), size() )
+#define minheap         priority_queue<int,vi,greater<int>>  // min heap ( push(), pop(), top(), empty(), size() )
+
+
+#define setbits(x)      __builtin_popcountll(x) // return the count of set bits
+#define startzeroes(x)  __builtin_clzll(x) //return the count the leading zeros of the integer(clz=count leading zero’s)
+#define endzeroes(x)    __builtin_ctzll(x) // used to count the trailing zeros of the given integer(ctz = count trailing zeros)
+
+#define ps(x,y)         fixed<<setprecision(y)<<x  // (always prefer double) works with float and double..ps(x,y)=x with y digits after decimal
+#define dma(a,n,type)   type *a=new type[n];  // dynamic memory allocation (Example : type*a = new int[n]; )
+
+const int inf   = numeric_limits<int>::max();
+const double pi = 3.1415926535898;
+const int mod   = 1000000007;
+const int lim   = 1000006;
+
+#define all(v)          v.begin(),v.end()
+#define sum(v)          accumulate(v.begin(),v.end(),(ll)0)
+#define asum(a,n)       accumulate(a,a+n,(ll)0)
+#define rev(v)          reverse(v.begin(),v.end());
+//  stoi() function - converts string to integer ( Example- int x = stoi(s) )
+//  stoll() funtion - converts string to long long ( Example- long long x = stoll(s) )
+//  to_string() function - converts integer to string ( Example- string s = to_string(x) )
+
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+}
+
+// lim = 10*6, array for taking input
+int a[lim];
+
+// Array for storing the Segment Tree
+// size should be 4*n, so making it of 4 * 10^6
+int segtree[4 * lim];
+
+// LazyTree for lazy propagation
+int lazy[4 * lim];
+
+// These 3 parameters are always there in any Segment Tree function
+// s_ind = segment index
+// ss = segment start
+// se = segment end
+
+// qs = query start
+// qe = qeuery end
+
+
+// Function to make the segment tree array before processing queries
+void build(int s_ind, int ss, int se) {
+
+    // when segment has only 1 element
+    if (ss == se) {
+        segtree[s_ind] = a[ss];
+        return;
+    }
+
+    int mid = (ss + se) / 2;
+
+    // left subtree
+    build(2 * s_ind, ss, mid);
+
+    // right subtree
+    build(2 * s_ind + 1, mid + 1, se);
+
+    // getting the result from left subtree and right subtree
+    segtree[s_ind] = segtree[2 * s_ind] + segtree[2 * s_ind + 1];
+
+}
+
+// Range Update ( Lazy Propagation )
+void update(int s_ind, int ss, int se, int qs, int qe, int val) {
+
+    // for addressing any pending updates before this current update
+    if (lazy[s_ind] != 0) {
+        int dx = lazy[s_ind];
+        lazy[s_ind] = 0;
+
+        segtree[s_ind] += dx * (se - ss + 1);
+
+        if (ss != se) {
+            lazy[2 * s_ind] += dx;
+            lazy[2 * s_ind + 1] += dx;
+        }
+    }
+
+    // Now moving on with the current update
+
+    // completely outside
+    if (qs > se or qe < ss)
+        return;
+
+    // completely inside ( segment tree completely inside the query range )
+    if (ss >= qs and se <= qe) {
+
+        int dx = (se - ss + 1) * val;
+        segtree[s_ind] += dx;
+
+        if (ss != se) {
+            lazy[2 * s_ind] += val;
+            lazy[2 * s_ind + 1] += val;
+        }
+
+        return;
+    }
+
+    int mid = (ss + se) / 2;
+
+    update(2 * s_ind, ss, mid, qs, qe, val);
+    update(2 * s_ind + 1, mid + 1, se, qs, qe, val);
+
+    segtree[s_ind] = segtree[2 * s_ind] + segtree[2 * s_ind + 1];
+
+}
+
+// Function to answer a query in O(logN) time ( brute force takes O(N) to process a single query )
+int query(int s_ind, int ss, int se, int qs, int qe) {
+
+    // for addressing any pending updates before this current update
+    if (lazy[s_ind] != 0) {
+        int dx = lazy[s_ind];
+        lazy[s_ind] = 0;
+
+        segtree[s_ind] += dx * (se - ss + 1);
+
+        if (ss != se) {
+            lazy[2 * s_ind] += dx;
+            lazy[2 * s_ind + 1] += dx;
+        }
+    }
+
+    // completely outside
+    if (qs > se or qe < ss)
+        return 0;
+
+    // completely inside ( segment completely inside the query range )
+    if (ss >= qs and se <= qe)
+        return segtree[s_ind];
+
+    int mid = (ss + se) / 2;
+
+    return query(2 * s_ind, ss, mid, qs, qe) + query((2 * s_ind) + 1, mid + 1, se, qs, qe);
+}
+
+
+int32_t main()
+{
+
+    fio;
+    int n;
+    cin >> n;
+
+    fo(i, 1, n)
+    cin >> a[i];
+
+    // Building the segment tree array
+    build(1, 1, n);
+
+    // number of queries
+    int q;
+    cin >> q;
+    while (q--) {
+
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            // Query is to give the sum in the range L to R
+
+            int qs, qe;
+            cin >> qs >> qe;
+            cout << query(1, 1, n, qs, qe) << endl;
+
+        }
+        else {
+            // Query is to update the range L to R ( add val to elements in range L to R )
+            int qs, qe, val;
+            cin >> qs >> qe >> val;
+            update(1, 1, n, qs, qe, val);
+
+        }
+
+    }
+
+}
