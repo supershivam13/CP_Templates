1b15d6b6a5940339d0317d9068a6f6ecfa92b99b
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-06-21 05:40:55 +0530

    Update Sliding_Window.cpp

--- Strings/Sliding_Window.cpp
+++ Strings/Sliding_Window.cpp
@@ -9,35 +9,41 @@
 #define mod (int)1e9+7
 using namespace std;
 
+// Time Complexity - O(N*M)
+// where N,M are the lengths of the text string and the pattern string
+
+// T = Text, P = Pattern to match
 string T , P;
 int LP , LT;
 
+// function to check whether the window sting matches the patter or not
 bool match(int L , int R)
 {
-	for(int i=0;i<LP;i++)
-	if(T[L+i] != P[i]) return false;
-	
-	return true;
+    for (int i = 0; i < LP; i++)
+        if (T[L + i] != P[i]) return false;
+
+    return true;
 }
 
 int main()
 {
-	int t;
-	cin>>t;
-	
-	cin.ignore();
-	
-	while(t--)
-	{
-		getline(cin , T);
-		getline(cin , P);
-		LP = P.size();
-		LT = T.size();
-		
-		for(int L=0 , R=LP-1; R<LT; L++ , R++)
-		if(match(L , R))
-		cout<<L<<" ";
-		
-		cout<<endl;
-	}
+    int t;
+    cin >> t;
+
+    cin.ignore();
+
+    while (t--)
+    {
+        getline(cin , T);
+        getline(cin , P);
+        LP = P.size();
+        LT = T.size();
+
+        for (int L = 0 , R = LP - 1; R < LT; L++ , R++)
+
+            if (match(L , R))
+                cout << L << " ";
+
+        cout << endl;
+    }
 }
