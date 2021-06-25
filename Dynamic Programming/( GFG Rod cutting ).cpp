9bc23a64f2dfdb815dcca540f5525a393ff9ebfc
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-06-25 18:35:30 +0530

    Create ( GFG Rod cutting ).cpp

---
+++ Dynamic Programming/( GFG Rod cutting ).cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int cutRod(int price[], int n) {
+
+        // Array which contains the permissible length of the pieces
+        // (In this question, all lengths from 1 to N are permissible )
+        // (Otherwise, this array will also be given as input )
+        int a[n];
+        for (int i = 0; i < n; i++)
+            a[i] = i + 1;
+
+        // DP Array ( dp[size of array + 1][length of rod + 1])
+        int dp[n + 1][n + 1];
+
+        // Initialising the dp array
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 or j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // if permssible length of piece is greater than the rod itself
+                if (a[i - 1] > j)
+                    dp[i][j] = dp[i - 1][j];
+                // else max of (included, not included)
+                else
+                    dp[i][j] = max(price[i - 1] + dp[i][j - a[i - 1]], dp[i - 1][j]);
+            }
+        }
+
+        return dp[n][n];
+    }
+};
