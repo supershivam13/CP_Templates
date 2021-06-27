f1e635fbddf06414ee2fc05f06085428e11bfd33
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-06-27 14:43:32 +0530

    Update Longest Common Substring or Subarray ( LeetCode Maximum Length of Repeated Subarray ).cpp

--- Dynamic Programming/Longest Common Substring or Subarray ( LeetCode Maximum Length of Repeated Subarray ).cpp
+++ Dynamic Programming/Longest Common Substring or Subarray ( LeetCode Maximum Length of Repeated Subarray ).cpp
@@ -1,1 +1,36 @@
+int dp[1001][1001];
 
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        // to store the result
+        int result = 0;
+
+        // Initialisation ( Base Condition )
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 or j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // if the element of both strings/arrays matches
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 0;
+
+                // storing the max value obtained in the whole dp matrix as LC Substring/Subarray can occur anywhere
+                result = max(result, dp[i][j]);
+            }
+        }
+
+        return result;
+    }
+};
