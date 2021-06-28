4692b3aa6a33142cc645e7ab5cd7abd2ff66804c
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-06-28 11:36:48 +0530

    Create Longest Palindromic Subsequence ( LeetCode LPS ).cpp

---
+++ Dynamic Programming/Longest Palindromic Subsequence ( LeetCode LPS ).cpp
@@ -0,0 +1,41 @@
+#define f(i,n)          for(int i=0;i<n;i++)
+#define F(i,a,n)        for(int i=a;i<n;i++)
+#define fo(i,a,b)       for(int i=a;i<=b;i++)
+
+int dp[1001][1001];
+
+// LPS (string x) = LCS( string x, string y ) [ where y is reverse string of x ]
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string x) {
+
+        string y = x;
+        reverse(y.begin(), y.end());
+
+        //Now same as finding LCS(x,y)
+        int m = x.length();
+        int n = y.length();
+
+        // Initialisation ( Base condition )
+        fo(i, 0, m) {
+            fo(j, 0, n) {
+                if (i == 0 or j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+
+        fo(i, 1, m) {
+            fo(j, 1, n) {
+                // if character of both strings matches
+                if (x[i - 1] == y[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+
+        return dp[m][n];
+
+    }
+};
