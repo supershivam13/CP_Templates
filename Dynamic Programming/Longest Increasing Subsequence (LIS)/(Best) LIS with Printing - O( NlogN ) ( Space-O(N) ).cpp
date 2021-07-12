873b97720ef2113ddbc1903f7188a3e9f460d744
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-07-12 17:16:36 +0530

    Create (Best) LIS with Printing - O( NlogN ) ( Space-O(N) ).cpp

---
+++ Dynamic Programming/Longest Increasing Subsequence (LIS)/(Best) LIS with Printing - O( NlogN ) ( Space-O(N) ).cpp
@@ -0,0 +1,141 @@
+/* Dynamic Programming C++ implementation
+of LIS problem */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+#define f(i,n)          for(int i=0;i<n;i++)
+#define F(i,a,n)        for(int i=a;i<n;i++)
+#define fo(i,a,b)       for(int i=a;i<=b;i++)
+#define arrin(a,n)      for(int i=0;i<n;i++)cin>>a[i]
+#define arrout(a,n)     for(int i=0;i<n;i++)cout<<a[i]<<" "
+#define print2d(a,n,m)  for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<"\n";}
+#define w(x)            int x; cin>>x; while(x--)
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define double          long double
+#define eb              emplace_back
+#define pb              push_back
+#define pf              push_front
+#define popb            pop_back
+#define popf            pop_front
+#define mp              make_pair
+#define rt              return
+#define br              break
+#define ct              continue
+#define elif            else if
+
+#define dbg             cout<<"\nhuehue\n"
+#define sayy            cout<<"YES\n"
+#define sayn            cout<<"NO\n"
+#define debug(a)        cerr << #a << ": " << a << endl
+
+
+#define pii             pair<int,int>  // ( p.first , p.second )
+#define pss             pair<string,string>
+
+#define mii             map<int,int> // ( insert(key,value), begin(), end(), size(), empty() )
+#define mci             map<char, int>
+#define msi             map<string, int>
+#define mss             map<string, string>
+#define umii            unordered_map<int,int>
+// Itertating over a map m ==>> for( auto i : m ){ cout<<i.first<<" "<<i.second<<endl; }
+
+#define vi              vector<int>  // ( size(), resize(), begin(), end(), empty() )
+#define vb              vector<bool>
+#define vii             vector<pair<int,int>>
+#define vvi             vector<vector<int>>
+
+#define si              set<int>  // stores in ascending order // ( insert(), size(), empty(), begin(), end() )
+#define sid             set<int, greater<int> >  // stores in descending order
+#define sii             set<pair<int,int>>
+
+#define li              list <int> // ( pb(), pf(), popb(), popf(), reverse(), sort(), empty(), size(),
+//  remove(), front(), back() insert() )
+
+#define maxheap         priority_queue<int>  // by default, max heap ( push(), pop(), top(), empty(), size() )
+#define minheap         priority_queue<int,vi,greater<int>>  // min heap ( push(), pop(), top(), empty(), size() )
+
+
+#define setbits(x)      __builtin_popcountll(x) // return the count of set bits
+#define startzeroes(x)  __builtin_clzll(x) //return the count the leading zeros of the integer(clz=count leading zero’s)
+#define endzeroes(x)    __builtin_ctzll(x) // used to count the trailing zeros of the given integer(ctz = count trailing zeros)
+
+#define ps(x,y)         fixed<<setprecision(y)<<x  // (always prefer double) works with float and double..ps(x,y)=x with y digits after decimal
+#define dma(a,n,type)   type *a=new type[n];  // dynamic memory allocation (Example : type*a = new int[n]; )
+
+const int inf   = numeric_limits<int>::max();
+const double pi = 3.1415926535898;
+const int mod   = 1000000007;
+const int lim   = 1000006;    // lim = 10^6 + 6
+
+#define all(v)          v.begin(),v.end()
+#define sum(v)          accumulate(v.begin(),v.end(),(ll)0)
+#define asum(a,n)       accumulate(a,a+n,(ll)0)
+#define rev(v)          reverse(v.begin(),v.end());
+
+
+// LONGEST INCREASING SUBSEQUENCE - DP + BinarySearch
+// TIME COMPLEXITY - O( NlogN )
+// SPACE COMPLEXITY - O( N )
+
+
+// Method -
+// We are keeping the smallest elements possible in our LIS so that more possible numbers can be
+// inserted next in the LIS.
+
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int a[1000001];
+
+// LIS() returns the vector containing LIS of the array a[] of size n
+vector<int> LIS(int n) {
+
+    vector<int> result;
+    result.pb(a[0]);
+
+    for (int i = 1; i < n; i++) {
+
+        // if a[i] is greater than the last element of LIS, then include it also in the LIS
+        if (result.back() < a[i])
+            result.pb(a[i]);
+        else {
+
+            // finding the index of the element just greater than or equal to a[i]
+            // and replacing it with a[i]
+            int ind = lower_bound(all(result), a[i]) - result.begin();
+            result[ind] = a[i];
+        }
+    }
+
+    return result;
+}
+
+int32_t main()
+{
+
+    fio;
+
+    c_p_c();
+
+    int n;
+    cin >> n;
+    arrin(a, n);
+
+    vector<int> ans = LIS(n);
+    cout << "Length of the Longest Increaasing Subsequence is " << ans.size() << endl;
+    arrout(ans, ans.size());
+
+    return 0;
+}
