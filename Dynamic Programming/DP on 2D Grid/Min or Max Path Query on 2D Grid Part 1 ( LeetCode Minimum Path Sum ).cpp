20a08977a137a11c5a0eac3cac79b0e4ba3f505f
supershivam13 <shivam_1901cs55@iitp.ac.in>
2021-07-12 22:57:27 +0530

    2d

---
+++ Dynamic Programming/DP on 2D Grid/Min or Max Path Query on 2D Grid Part 1 ( LeetCode Minimum Path Sum ).cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& a) {
+
+        // n = number of rows, m = number of columns
+        int n = a.size();
+        int m = a[0].size();
+
+        // 1-based indexing and 1 extra row and column for base condition
+        int dp[n + 2][m + 2];
+
+        // Pre-processing (since we have to find the minimum , so initialising the extra row with INT_MAX )
+        for (int i = 1; i <= m + 1; i++)
+            dp[n + 1][i] = INT_MAX;
+
+        // Pre-processing (since we have to find the minimum , so initialising the extra row with INT MAX )
+        for (int i = 1; i <= n + 1; i++)
+            dp[i][m + 1] = INT_MAX;
+
+        // Since cost to reach (n,m) from (n,m) is just a[n][m], so that min of both is zero
+        dp[n + 1][m] = dp[n][m + 1] = 0;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = m; j >= 1; j--) {
+                // cost to reach (n,m) from (i,j) is a[i][j] + min(right, down)
+                dp[i][j] = a[i - 1][j - 1] + min(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+
+        return dp[1][1];
+
+    }
+};
