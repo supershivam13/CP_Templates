f25b18ac0aa9cdd1e259a9729c02f7a30a459928
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-07-13 17:40:24 +0530

    Update ( Leetcode Coin Change - Min # of coins ).cpp

--- Dynamic Programming/( Leetcode Coin Change - Min # of coins ).cpp
+++ Dynamic Programming/( Leetcode Coin Change - Min # of coins ).cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        
-        int dp[amount+1]; 
-        for(int i=0;i<=amount;i++)
-            dp[i]=100000000;
-        
-        dp[0]=0;
-        
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.size();j++){
-                if(i-coins[j]>=0)
-                    dp[i]=min(dp[i],dp[i-coins[j]]+1);
+
+        // 1-based indexing
+
+        // Initialising DP array with INT_MAX;
+        long long int dp[amount + 1];
+        for (int i = 0; i <= amount; i++)
+            dp[i] = INT_MAX;
+
+        // Zero coins are needed to make a sum of zero
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i - coins[j] >= 0)
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
             }
         }
-        
-        if(dp[amount]==100000000)
+
+        // if amount can't be formed using the given coins
+        if (dp[amount] == INT_MAX)
             return -1;
         else
-            return dp[amount];   
+            return dp[amount];
     }
 };
