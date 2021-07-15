77176807f6ee3a1c1fe97a3f4b51aa5979e502f6
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-07-15 12:22:39 +0530

    Create Minimum Subarray Sum - Modified Kadane's Algorithm - O(N).cpp

---
+++ Dynamic Programming/Minimum Subarray Sum - Modified Kadane's Algorithm - O(N).cpp
@@ -0,0 +1,163 @@
+/*
+    Author      : SHIVAM SAHU
+    Institution : IIT PATNA
+    Country     : INDIA
+*/
+
+#include<bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+// PRAGMA reduces execution time a little
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+
+// The typedef is used to give data type a new name
+// #define in C is a directive which is used to #define alias(shortcuts)
+typedef long long ll;
+typedef long double ld;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+#define f(i,n)          for(int i=0;i<n;i++)
+#define F(i,a,n)        for(int i=a;i<n;i++)
+#define fo(i,a,b)       for(int i=a;i<=b;i++)
+#define arrin(a,n)      for(int i=0;i<n;i++)cin>>a[i]
+#define arrout(a,n)     for(int i=0;i<n;i++)cout<<a[i]<<" "
+#define print2d(a,n,m)  for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<"\n";}
+#define w(x)            int x; cin>>x; while(x--)
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define double          long double
+#define eb              emplace_back
+#define pb              push_back
+#define pf              push_front
+#define popb            pop_back
+#define popf            pop_front
+#define mp              make_pair
+#define rt              return
+#define br              break
+#define ct              continue
+#define elif            else if
+
+#define dbg             cout<<"\nhuehue\n"
+#define sayy            cout<<"YES\n"
+#define sayn            cout<<"NO\n"
+#define debug(a)        cerr << #a << ": " << a << endl
+
+
+#define pii             pair<int,int>  // ( p.first , p.second )
+#define pss             pair<string,string>
+
+#define mii             map<int,int> // ( insert(key,value), begin(), end(), size(), empty() )
+#define mci             map<char, int>
+#define msi             map<string, int>
+#define mss             map<string, string>
+#define umii            unordered_map<int,int>
+// Itertating over a map m ==>> for( auto i : m ){ cout<<i.first<<" "<<i.second<<endl; }
+
+#define vi              vector<int>  // ( size(), resize(), begin(), end(), empty() )
+#define vb              vector<bool>
+#define vii             vector<pair<int,int>>
+#define vvi             vector<vector<int>>
+
+#define si              set<int>  // stores in ascending order // ( insert(), size(), empty(), begin(), end() )
+#define sid             set<int, greater<int> >  // stores in descending order
+#define sii             set<pair<int,int>>
+
+#define li              list <int> // ( pb(), pf(), popb(), popf(), reverse(), sort(), empty(), size(),
+//  remove(), front(), back() insert() )
+
+#define maxheap         priority_queue<int>  // by default, max heap ( push(), pop(), top(), empty(), size() )
+#define minheap         priority_queue<int,vi,greater<int>>  // min heap ( push(), pop(), top(), empty(), size() )
+
+
+#define setbits(x)      __builtin_popcountll(x) // return the count of set bits
+#define startzeroes(x)  __builtin_clzll(x) //return the count the leading zeros of the integer(clz=count leading zero’s)
+#define endzeroes(x)    __builtin_ctzll(x) // used to count the trailing zeros of the given integer(ctz = count trailing zeros)
+
+#define ps(x,y)         fixed<<setprecision(y)<<x  // (always prefer double) works with float and double..ps(x,y)=x with y digits after decimal
+#define dma(a,n,type)   type *a=new type[n];  // dynamic memory allocation (Example : type*a = new int[n]; )
+
+const int inf   = numeric_limits<int>::max();
+const double pi = 3.1415926535898;
+const int mod   = 1000000007;
+const int lim   = 1000006;    // lim = 10^6 + 6
+
+#define all(v)          v.begin(),v.end()
+#define sum(v)          accumulate(v.begin(),v.end(),(ll)0)
+#define asum(a,n)       accumulate(a,a+n,(ll)0)
+#define rev(v)          reverse(v.begin(),v.end());
+//  stoi() function - converts string to integer ( Example- int x = stoi(s) )
+//  stoll() funtion - converts string to long long ( Example- long long x = stoll(s) )
+//  to_string() function - converts integer to string ( Example- string s = to_string(x) )
+
+
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+// MODIFIED KADANE'S ALGORITHM - FOR FINDING THE MINIMUM SUBARRAY SUM OF AN ARRAY
+
+// ( Only we change the signs of the elements of the input array, then apply the normal KADANE'S Algo
+// to find the maximum subarray sum and then return the result with a negative sign )
+
+// TIME COMPLEXITY - O(N)
+// SPACE COMPLEXITY - O(N)
+
+
+
+int a[100001];
+
+// DP Array
+int dp[100001];
+
+int32_t main()
+{
+    fio;
+    c_p_c();
+
+    int n;
+    cin >> n;
+
+    int z = 0;
+    dp[0] = 0;
+
+    // 1-based indexing
+    fo(i, 1, n)
+    cin >> a[i];
+
+    // First invert the sign of the elements
+    fo(i, 1, n)
+    a[i] = -a[i];
+
+
+    // Apply the normal Kadane algorithm But on the elements
+    // of the array having inverted sign
+
+    // DP[i] = maximum possible subarray sum ending at position i
+    // ( i.e dp[i] = maxisum subarray sum of all subarrays which are ending at position i )
+    fo(i, 1, n) {
+        // a[i] will always be there, adding the dp[i-1] only if it is greater than 0
+        dp[i] = max(dp[i - 1], z) + a[i];
+    }
+
+    // Invert  the answer to get minimum value
+    int result = -(*max_element(dp + 1, dp + n + 1));
+
+    cout << result << endl;
+
+}
