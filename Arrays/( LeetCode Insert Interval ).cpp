5cbc84402c489265f8bec20d164c9bd3b513a813
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-07-16 15:53:27 +0530

    Create ( LeetCode Insert Interval ).cpp

---
+++ Arrays/( LeetCode Insert Interval ).cpp
@@ -0,0 +1,82 @@
+#define f(i,n)          for(int i=0;i<n;i++)
+#define F(i,a,n)        for(int i=a;i<n;i++)
+#define fo(i,a,b)       for(int i=a;i<=b;i++)
+#define arrin(a,n)      for(int i=0;i<n;i++)cin>>a[i]
+#define arrout(a,n)     for(int i=0;i<n;i++)cout<<a[i]<<" "
+#define print2d(a,n,m)  for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<"\n";}
+#define w(x)            int x; cin>>x; while(x--)
+
+#define ff              first
+#define ss              second
+#define double          long double
+#define eb              emplace_back
+#define pb              push_back
+#define pf              push_front
+#define popb            pop_back
+#define popf            pop_front
+#define mp              make_pair
+#define rt              return
+#define br              break
+#define ct              continue
+#define elif            else if
+
+#define dbg             cout<<"\nhuehue\n"
+#define sayy            cout<<"YES\n"
+#define sayn            cout<<"NO\n"
+#define debug(a)        cerr << #a << ": " << a << endl
+
+
+#define pii             pair<int,int>  // ( p.first , p.second )
+#define pss             pair<string,string>
+
+#define mii             map<int,int> // ( insert(key,value), begin(), end(), size(), empty() )
+#define mci             map<char, int>
+#define msi             map<string, int>
+#define mss             map<string, string>
+#define umii            unordered_map<int,int>
+// Itertating over a map m ==>> for( auto i : m ){ cout<<i.first<<" "<<i.second<<endl; }
+
+#define vi              vector<int>  // ( size(), resize(), begin(), end(), empty() )
+#define vb              vector<bool>
+#define vii             vector<pair<int,int>>
+#define vvi             vector<vector<int>>
+
+#define si              set<int>  // stores in ascending order // ( insert(), size(), empty(), begin(), end() )
+#define sid             set<int, greater<int> >  // stores in descending order
+#define sii             set<pair<int,int>>
+
+
+class Solution {
+public:
+    vector<vector<int>> insert(vvi& intervals, vi& newInterval) {
+
+        vvi ans;
+
+        int i = 0;
+        int n = intervals.size();
+
+        // adding all the non-overlapping intervals before the newInterval
+        while (i < n and intervals[i][1] < newInterval[0]) {
+            ans.pb(intervals[i]);
+            i++;
+        }
+
+        // finding the newInterval to be added
+        while (i < n and intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        // finally adding the newInterval
+        ans.pb(newInterval);
+
+        //adding all the remaining intervals to the ans vector
+        while (i < n) {
+            ans.pb(intervals[i]);
+            i++;
+        }
+
+        return ans;
+
+    }
+};
