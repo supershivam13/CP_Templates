b00ea1138b4c994e5408eb32643426488a17346a
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-02 02:16:23 +0530

    Update ( Imp ) Top K Frequent Elements in Array ( GFG ).cpp

--- Heaps/( Imp ) Top K Frequent Elements in Array ( GFG ).cpp
+++ Heaps/( Imp ) Top K Frequent Elements in Array ( GFG ).cpp
@@ -13,21 +13,6 @@
 // so, pushing an elements take logK time, so overall TC - O( NlogK ) )
 
 
-// HEAP QUESTIONS
-
-// IDENTIFICATION
-// Two keywords occurs together - 1) K                2) Largest/Smallest
-
-// a) K + Largest =  MIN HEAP
-// b) K + Smallest = MAX_HEAP
-
-// **********************************************************************************************
-
-// Time Complexity - O( NlogK )
-// ( A for loop of N complexity and we never stores more than k elements in heap at any moment
-// so, pushing an elements take logK time, so overall TC - O( NlogK ) )
-
-
 class Solution {
 public:
     vector<int> topK(vector<int>& a, int k) {
