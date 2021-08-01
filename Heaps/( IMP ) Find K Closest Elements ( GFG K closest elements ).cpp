f4ee43ee64143b7a35327a23cbbc5398715c0fef
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-01 22:54:40 +0530

    Create ( IMP ) Find K Closest Elements ( GFG K closest elements ).cpp

---
+++ Heaps/( IMP ) Find K Closest Elements ( GFG K closest elements ).cpp
@@ -0,0 +1,54 @@
+// HEAP QUESTIONS
+
+// IDENTIFICATION
+// Two keywords occurs together - 1) K                2) Largest/Smallest
+
+// a) K + Largest =  MIN HEAP
+// b) K + Smallest = MAX_HEAP
+
+// **********************************************************************************************
+
+// Time Complexity - O( NlogK )
+// ( A for loop of N complexity and we never stores more than k elements in heap at any moment
+// so, pushing an elements take logK time, so overall TC - O( NlogK ) )
+
+
+class Solution {
+public:
+    vector<int> printKClosest(vector<int> a, int n, int k, int x) {
+
+        // vector of integers to store the results
+        vector<int> ans;
+
+        // making a max heap of pair { absolute difference, element }
+        priority_queue<pair<int, int>> mx;
+
+        for (int i = 0; i < n; i++) {
+
+            // acc. to question, If X is present in the array, then it need not be considered
+            if (abs(x - a[i]) == 0)
+                continue;
+
+            // sorting will be done on the basis of first element of pair
+            // in case first elements of pair are equal, then sorting done on basis of second
+
+            // acc. to question, we need larger element before in case of same absolute difference
+            // so, storing the second as negative, so to reverse the comparator function of sorting
+            mx.push({abs(x - a[i]), -a[i]});
+
+            if (mx.size() > k)
+                mx.pop();
+        }
+
+        // inserting the remaining elements of the heap to the ans vector
+        while (mx.size() > 0) {
+            ans.push_back(-1 * (mx.top().second));
+            mx.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+
+    }
+};
