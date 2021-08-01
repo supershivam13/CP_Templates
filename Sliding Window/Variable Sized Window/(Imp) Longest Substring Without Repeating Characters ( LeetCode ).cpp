530c5d78770a8cdaabc0ecbc24c0a29a8c892076
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-01 11:49:28 +0530

    Create (Imp) Longest Substring Without Repeating Characters ( LeetCode ).cpp

---
+++ Sliding Window/Variable Sized Window/(Imp) Longest Substring Without Repeating Characters ( LeetCode ).cpp
@@ -0,0 +1,55 @@
+// VARIABLE SIZED SLIDING WINDOW
+
+//  i = start of window
+//  j = end of window
+//  window size = j-i+1
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int n = s.length();
+        int i = 0, j = 0, ans = 0;
+
+        // map to store the count of characters in the current window
+        // and map.size() gives the number of unique characters
+        unordered_map<char, int> mp;
+
+        while (j < n) {
+
+            mp[s[j]]++;
+
+            // this if statement will never will be executed
+            // as it never will be true
+            if (mp.size() > j - i + 1) {
+                j++;
+            }
+
+            // when there is all unique characters in the window, then window size = map.size()
+            // so, it can be a potential candidate for ans
+            else if (mp.size() == j - i + 1) {
+                ans = max(ans, j - i + 1);
+                j++;
+            }
+
+            // when there is some repeating characters in the window, so removing elements till the
+            // window size = map.size()
+            else {
+
+                while (mp.size() < j - i + 1) {
+                    mp[s[i]]--;
+
+                    if (mp[s[i]] == 0)
+                        mp.erase(s[i]);
+
+                    i++;
+                }
+
+                j++;
+            }
+        }
+        return ans;
+
+    }
+};
