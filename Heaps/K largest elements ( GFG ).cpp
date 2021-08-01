cf5f49894b702345e6d57baafac52dec7f5ce25a
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-01 19:09:09 +0530

    Create K largest elements ( GFG ).cpp

---
+++ Heaps/K largest elements ( GFG ).cpp
@@ -0,0 +1,44 @@
+// HEAP QUESTIONS
+
+// IDENTIFICATION
+// Two keywords occurs together - 1) K                2) Largest/Smallest
+
+// a) K + Largest =  MIN HEAP
+// b) K + Smallest = MAX_HEAP
+
+// ****************************************************************************************
+
+
+// Time Complexity - O( NlogK )
+// ( A for loop of N complexity and we never stores more than k elements in heap at any moment
+// so, pushing an elements take logK time, so overall TC - O( NlogK ) )
+
+#define minheap         priority_queue<int,vector<int>,greater<int>>
+
+class Solution
+{
+public:
+    vector<int> kLargest(int arr[], int n, int k)
+    {
+        // vector of integers to store the results
+        vector<int> ans;
+        minheap minh;
+
+        for (int i = 0; i < n; i++) {
+            minh.push(arr[i]);
+
+            // only keeping the largest k elements in the heap
+            if (minh.size() > k)
+                minh.pop();
+        }
+
+        // inserting the elements of the heap to the ans vector
+        while (k--) {
+            ans.push_back(minh.top());
+            minh.pop();
+        }
+
+        return ans;
+    }
+
+};
