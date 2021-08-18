f86f2c9ceccb90e22947fb52d59c46bd0e28c1a2
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-19 02:55:40 +0530

    Create Search in Row wise and Column wise Sorted Matrix ( LeetCode Search a 2D Matrix ).cpp

---
+++ Arrays/Search in Row wise and Column wise Sorted Matrix ( LeetCode Search a 2D Matrix ).cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& a, int x) {
+
+        // m = no. of rows and n = no. of columns
+        int m = a.size();
+        int n = a[0].size();
+
+        // Starting from the top right corner( it acts as the mid)
+        int i = 0;
+        int j = n - 1;
+
+        // while i and j are within the bounds of the matrix
+        while (i<m and j >= 0) {
+
+            if (a[i][j] == x)
+                return true;
+
+            // since the matrix is row-column wise sorted, so if a[i][j] > target,
+            // then moving one column backwards
+            else if (a[i][j] > x)
+                j--;
+            // else moving one row downside
+            else
+                i++;
+        }
+
+        return false;
+    }
+};
