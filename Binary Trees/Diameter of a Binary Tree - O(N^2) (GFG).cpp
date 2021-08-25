c72f2b6a9235d17acd953e4bc4c77c289ae56f69
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-26 03:23:36 +0530

    Create Diameter of a Binary Tree - O(N^2) (GFG).cpp

---
+++ Binary Trees/Diameter of a Binary Tree - O(N^2) (GFG).cpp
@@ -0,0 +1,49 @@
+// Time Complexity - O(N*N)
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+*/
+
+class Solution {
+public:
+
+    int height(Node* root) {
+
+        if (root == NULL)
+            return 0;
+
+        return 1 + max(height(root->left), height(root->right));
+    }
+
+    // Solution starts here
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+
+        if (root == NULL)
+            return 0;
+
+        // when diameter passes through root
+        int d1 = height(root->left) + height(root->right) + 1 ;
+
+        // when diameter lies in the left subtreee
+        int d2 = diameter(root->left);
+
+        // when diameter lies in the left subtreee
+        int d3 = diameter(root->right);
+
+        return max(d1, max(d2, d3));
+
+    }
+};
