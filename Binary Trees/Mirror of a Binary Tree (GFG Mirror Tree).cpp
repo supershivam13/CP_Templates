be3fcf550bab7c998754e8f09dcc42150b59e350
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-27 02:39:05 +0530

    Create Mirror of a Binary Tree (GFG Mirror Tree).cpp

---
+++ Binary Trees/Mirror of a Binary Tree (GFG Mirror Tree).cpp
@@ -0,0 +1,30 @@
+// Time Complexity - O(N)
+// Space Complexity - O( Height of tree ) { used in storing recursive calls in stack }
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; 
+
+*/
+
+class Solution {
+  public:
+    void mirror(Node* node) {
+        
+        if(node==NULL)
+            return;
+         
+         mirror(node->left);
+         mirror(node->right);
+         swap(node->left,node->right);
+         
+    }
+};
