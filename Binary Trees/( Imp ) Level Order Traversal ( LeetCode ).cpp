d8bddaf63633f00306bbbd4cd8da7d3f037a5638
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-28 19:46:53 +0530

    Update ( Imp ) Level Order Traversal ( LeetCode ).cpp

--- Binary Trees/( Imp ) Level Order Traversal ( LeetCode ).cpp
+++ Binary Trees/( Imp ) Level Order Traversal ( LeetCode ).cpp
@@ -1,16 +1,6 @@
-/*
+// Level Order Traversal is an example of PreOrder Traversal.
 
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
 
- */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
