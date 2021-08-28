03f5fa82d88661bff272f81d8b720bccd3753643
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-28 23:08:53 +0530

    Update and rename Top View of Binary Tree (GFG).cpp to Top and Bottom View of Binary Tree (GFG).cpp

---
+++ Binary Trees/Top and Bottom View of Binary Tree (GFG).cpp
@@ -0,0 +1,93 @@
+// For both Top and Bottom View
+// Time Complexity - O( N*logN ){ logN is used in storing in map and N is for traversing every node one time}
+// Space Complexity  - O(N) { as we are using a queue to store }
+
+
+
+// *****************************************************************************************
+
+// Top View of Binary Tree
+
+// Should use Level Order Traversal (not any Pre,Post,In) in this question as Level order gives the first
+// occurence of value at x-coordinate which is first as well as top element at that x-coordinate too.
+
+class Solution {
+public:
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+
+        // map of {value,x-cordinate} to store the only first occurence at each new value of x
+        map<int, int> m;
+
+        // Perform Level Order traversal
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+
+            Node* temp = q.front().first;
+            int x = q.front().second;
+            q.pop();
+
+            // only store the first occurence of 'x' along with its value as {value,x}
+            if (m.find(x) == m.end())
+                m[x] = temp->data;
+
+            if (temp->left)
+                q.push({temp->left, x - 1});
+            if (temp->right)
+                q.push({temp->right, x + 1});
+        }
+
+        for (auto p : m)
+            ans.push_back(p.second);
+
+        return ans;
+    }
+};
+
+
+// **************************************************************************************root
+
+// Bottom View of Binary Tree
+
+// Should use Level Order Traversal (not any Pre,Post,In) in this question as If there are multiple
+// bottom-most nodes for a 'x, then we have to print the later one in level traversal
+
+
+class Solution {
+public:
+    vector<int> bottomView(Node *root)
+    {
+        vector<int> ans;
+
+        // map of {value,x-cordinate} to store the only last occurence at each value of x
+        map<int, int> m;
+
+        // Perform Level Order traversal
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+
+            Node* temp = q.front().first;
+            int x = q.front().second;
+            q.pop();
+
+            // Only difference with Top View is here, we are not checking any condition on map
+            // we just keep on updating the values in map as we want the last or bottom view
+            m[x] = temp->data;
+
+            if (temp->left)
+                q.push({temp->left, x - 1});
+            if (temp->right)
+                q.push({temp->right, x + 1});
+        }
+
+        for (auto p : m)
+            ans.push_back(p.second);
+
+        return ans;
+    }
+};
