d1198f19dd968bb210bc9fe222d70ff8f875a505
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-30 03:44:31 +0530

    Update Insert a node in a BST (GFG).cpp

--- Binary Search Trees (BST)/Insert a node in a BST (GFG).cpp
+++ Binary Search Trees (BST)/Insert a node in a BST (GFG).cpp
@@ -1,16 +1,20 @@
 //Function to insert a node in a BST.
-
 Node* insert(Node* root, int key)
 {
-    if(root==NULL){
+    if (root == NULL) {
         Node *newroot = new Node(key);
         return newroot;
     }
-    
-    if(root->data>key)
-        root->left = insert(root->left,key);
-    else if(root->data<key)
-        root->right = insert(root->right,key);
-    
+
+    // if root->data > key, then call on the left subtree recursively
+    if (root->data > key)
+        root->left = insert(root->left, key);
+
+    // See if root->data = key, then we are doing nothing
+
+    // else if root->data < key, then call on the left subtree recursively
+    else if (root->data < key)
+        root->right = insert(root->right, key);
+
     return root;
 }
