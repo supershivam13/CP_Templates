39bd16660e9d978abc97d19fa46fc5a77ea96b49
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-08-29 22:51:38 +0530

    Update and rename Mirror of a Binary Tree (GFG Mirror Tree).cpp to Mirror ( Invert ) of a Binary Tree (GFG Mirror Tree).cpp

--- Binary Trees/Mirror ( Invert ) of a Binary Tree (GFG Mirror Tree).cpp
+++ Binary Trees/Mirror ( Invert ) of a Binary Tree (GFG Mirror Tree).cpp
@@ -1,22 +1,6 @@
 // Time Complexity - O(N)
 // Space Complexity - O( Height of tree ) { used in storing recursive calls in stack }
 
-/*
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-}; 
-
-*/
-
 class Solution {
   public:
     void mirror(Node* node) {
