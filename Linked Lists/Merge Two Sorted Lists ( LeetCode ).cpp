6c6149585f14011170cef6e548abf03a4abda509
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-09-06 17:09:50 +0530

    Update Merge Two Sorted Lists ( LeetCode ).cpp

--- Linked Lists/Merge Two Sorted Lists ( LeetCode ).cpp
+++ Linked Lists/Merge Two Sorted Lists ( LeetCode ).cpp
@@ -1,11 +1,65 @@
+// Method 1 - Iterative method ( similar to Merge two Sorted arrays )
+
 // Time Complexity - O(N + M)
-// Space Complexity - O(N + M)
+// Space Complexity - O( 1 )
 
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
 
-        // if either one list is empty, return the other list
+        if (a == NULL)
+            return b;
+        if (b == NULL)
+            return a;
+
+        ListNode* p1 = a;
+        ListNode* p2 = b;
+        ListNode* dummyNode = new ListNode();
+        ListNode* p3 = dummyNode;
+
+        while (p1 != NULL and p2 != NULL) {
+
+            if (p1->val < p2->val) {
+                p3->next = p1;
+                p1 = p1->next;
+            }
+            else {
+                p3->next = p2;
+                p2 = p2->next;
+            }
+            p3 = p3->next;
+        }
+
+        while (p1 != NULL) {
+            p3->next = p1;
+            p1 = p1->next;
+            p3 = p3->next;
+        }
+
+        while (p2 != NULL) {
+            p3->next = p2;
+            p2 = p2->next;
+            p3 = p3->next;
+        }
+
+        ListNode* newhead = dummyNode->next;
+
+        return newhead;
+    }
+};
+
+
+
+
+// Method 2 - Recursive Method
+
+// Time Complexity - O(N + M)
+// Space Complexity - O(N + M) ( to store the recursive calls in stack )
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+
         if (a == NULL)
             return b;
         if (b == NULL)
