6e564c7d7f060aeb6410cea2d847aac1758cf1cb
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-09-09 01:22:15 +0530

    Update ( Imp )Median of Two Sorted Arrays ( LeetCode ).cpp

--- Binary Search/( Imp )Median of Two Sorted Arrays ( LeetCode ).cpp
+++ Binary Search/( Imp )Median of Two Sorted Arrays ( LeetCode ).cpp
@@ -32,18 +32,14 @@ public:
         if (nums1.size() > nums2.size())
             return findMedianSortedArrays(nums2, nums1);
 
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        int n1 = nums1.size(), int n2 = nums2.size();
 
-        // low, high indicates the minimum and maximum elements that can be taken from the nums1 array
-        // in the partition
+        // low, high indicates the minimum and maximum elements that can be taken from the nums1 array in the partition
         int low = 0, high = n1;
 
-        // Binary Search
         while (low <= high) {
 
             int cut1 = low + ((high - low) / 2);
-
             // 'cut1' number of elements are taken from nums1 array, so 'cut2' number of elements
             // should be taken from the nums2 array
             int cut2 = ((n1 + n2 + 1) / 2) - cut1;
@@ -51,7 +47,6 @@ public:
             // Handling corner cases
             int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
             int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
-
             // Handling corner cases
             int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
             int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
@@ -68,10 +63,8 @@ public:
             // need to go left to make left1 smaller than right2
             else if (left1 > right2)
                 high = cut1 - 1;
-
             else
                 low = cut1 + 1;
-
         }
         return 0.0;
     }
