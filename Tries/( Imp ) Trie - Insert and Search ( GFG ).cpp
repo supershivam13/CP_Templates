81797e714818206c2ed1749b293fb5e308db806a
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-09-08 20:43:35 +0530

    Update ( Imp ) Trie - Insert and Search ( GFG ).cpp

--- Tries/( Imp ) Trie - Insert and Search ( GFG ).cpp
+++ Tries/( Imp ) Trie - Insert and Search ( GFG ).cpp
@@ -59,18 +59,16 @@ void insert(struct TrieNode *root, string key)
         // only marking the node with end of the word as true
         if (i == key.length() - 1)
             temproot->isLeaf = true;
-
     }
 }
 
 //Function to use TRIE data structure and search the given string.
 bool search(struct TrieNode *root, string key)
-{
+{   
     // creating a copy of the main root node (as root node is the most important node of Trie)
     TrieNode* temproot = root;
 
     for (int i = 0; i < key.length(); i++) {
-
         int index = key[i] - 'a';
 
         // if word is not present, simply return false
@@ -80,6 +78,5 @@ bool search(struct TrieNode *root, string key)
         // moving to the next node
         temproot = temproot->children[index];
     }
-
     return temproot->isLeaf;
 }
