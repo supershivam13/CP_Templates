2e3ea61fa653b760c4ca3515ba8e59df29876d2f
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-09-09 00:19:10 +0530

    Update Closest Element in BST ( GFG ).cpp

--- Binary Search Trees (BST)/Closest Element in BST ( GFG ).cpp
+++ Binary Search Trees (BST)/Closest Element in BST ( GFG ).cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int minDiff(Node *root, int k) {
-
         // to store the closest element to k
         int closest;
         int diff = INT_MAX;
-
         Node* temp = root;
 
         // Traversing the BST from root to leaf
@@ -29,8 +27,6 @@ public:
             else
                 temp = temp->right;
         }
-
         return diff;
-
     }
 };
