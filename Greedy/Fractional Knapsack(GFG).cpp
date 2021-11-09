4c06ffef08760ec661010956cd412c136f70614e
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2021-11-09 15:26:52 +0530

    Create Fractional Knapsack(GFG).cpp

---
+++ Greedy/Fractional Knapsack(GFG).cpp
@@ -0,0 +1,39 @@
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+bool cmp(Item A,Item B)
+{
+    double r1=(double)A.value/(double)A.weight;
+    double r2=(double)B.value/(double)B.weight;
+    return r1>r2;
+}
+
+
+class Solution
+{
+    public:
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        sort(arr,arr+n,cmp);
+        double ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(W>=arr[i].weight)
+            {
+                W-=arr[i].weight;
+                ans+=arr[i].value;
+            }
+            else
+            {
+                double fraction=(W*1.0)/arr[i].weight;
+                ans+=arr[i].value*fraction;
+                break;
+            }   
+        }
+        return ans;
+    }
+};
