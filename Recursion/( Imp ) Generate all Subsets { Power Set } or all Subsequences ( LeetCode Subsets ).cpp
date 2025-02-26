43575b70ba8677933983f59b7406ac3f1caeefaf
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-02-27 02:24:27 +0530

    Update ( Imp ) Generate all Subsets { Power Set } or all Subsequences ( LeetCode Subsets ).cpp

--- Recursion/( Imp ) Generate all Subsets { Power Set } or all Subsequences ( LeetCode Subsets ).cpp
+++ Recursion/( Imp ) Generate all Subsets { Power Set } or all Subsequences ( LeetCode Subsets ).cpp
@@ -17,16 +17,14 @@ public:
     // Recursive function to generate all subsets of a vector { PowerSet }
     void subsets(vector<int> a, vector<int>out) {
 
-        // Base condition, when input length is zero, then we get a subset
-        // ( leaf node of the recursive tree )
+        // Base condition, when input length is zero, then we get a subset ( leaf node of the recursive tree )
         if (a.size() == 0) {
             ans.push_back(out);
             return;
         }
 
-        // copying the previous output for two recursive call functions
-        vector<int> out1 = out;
-        vector<int> out2 = out;
+        vector<int> out1 = out;   // copying the previous output for two recursive call functions
+        vector<int> out2 = out;   // copying the previous output for two recursive call functions
 
         // in first recursive call, we decide to not take the input in output, so no change in output
         // in second recursive call, we decide to take the input in output, so adding the first element of input
@@ -38,17 +36,13 @@ public:
         // Making two recursive calls with smaller inputs
         subsets(a, out1);
         subsets(a, out2);
-
     }
 
     // Solution starts here
     vector<vector<int>> subsets(vector<int>& a) {
 
-        // initialised output vector
-        vector<int> out;
-
-        // recursive function to generate all subsets of a vector { PowerSet }
-        subsets(a, out);
+        vector<int> out;  // initialised output vector
+        subsets(a, out);  // recursive function to generate all subsets of a vector { PowerSet }
 
         return ans;
     }
