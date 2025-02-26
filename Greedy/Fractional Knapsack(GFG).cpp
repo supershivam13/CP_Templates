1c3ddb7b606e70dd8909a7f43212861719dfc69f
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-02-26 20:44:09 +0530

    Update Fractional Knapsack(GFG).cpp

--- Greedy/Fractional Knapsack(GFG).cpp
+++ Greedy/Fractional Knapsack(GFG).cpp
@@ -1,33 +1,31 @@
-/*
-struct Item{
+
+/* struct Item{
     int value;
     int weight;
-};
-*/
+};   */
 
-bool cmp(Item A,Item B)
+bool cmp(Item A,Item B)   // sorting the array in descending order of value density
 {
     double r1=(double)A.value/(double)A.weight;
     double r2=(double)B.value/(double)B.weight;
     return r1>r2;
 }
 
-
 class Solution
 {
     public:
     double fractionalKnapsack(int W, Item arr[], int n)
     {
-        sort(arr,arr+n,cmp);
+        sort(arr,arr+n,cmp);   //  sorting the array using comparator function
         double ans=0;
         for(int i=0;i<n;i++)
         {
-            if(W>=arr[i].weight)
+            if(W>=arr[i].weight)   // when whole item can be taken
             {
                 W-=arr[i].weight;
                 ans+=arr[i].value;
             }
-            else
+            else                   // when the fraction of object is taken
             {
                 double fraction=(W*1.0)/arr[i].weight;
                 ans+=arr[i].value*fraction;
