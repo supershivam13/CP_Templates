ce087f3361e742a82440df2f5513993f151d9810
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-02-27 03:26:28 +0530

    Update Diameter of a Binary Tree - O(N^2) (GFG).cpp

--- Binary Trees/Diameter of a Binary Tree - O(N^2) (GFG).cpp
+++ Binary Trees/Diameter of a Binary Tree - O(N^2) (GFG).cpp
@@ -12,22 +12,16 @@ public:
     }
 
     // Solution starts here
-    // Function to return the diameter of a Binary Tree.
-    int diameter(Node* root) {
+    int diameter(Node* root) {    // Function to return the diameter of a Binary Tree.
 
         if (root == NULL)
             return 0;
 
-        // when diameter passes through root
-        int d1 = height(root->left) + height(root->right) + 1 ;
-
-        // when diameter lies in the left subtreee
-        int d2 = diameter(root->left);
-
-        // when diameter lies in the left subtreee
-        int d3 = diameter(root->right);
+        
+        int d1 = height(root->left) + height(root->right) + 1 ;   // when diameter passes through root
+        int d2 = diameter(root->left);                            // when diameter lies in the left subtreee
+        int d3 = diameter(root->right);                           // when diameter lies in the left subtreee
 
         return max(d1, max(d2, d3));
-
     }
 };
