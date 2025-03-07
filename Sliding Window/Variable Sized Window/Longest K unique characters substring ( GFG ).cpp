a486d4d80b25e88cdbe9b014f3ae873b28e785d0
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-08 00:42:26 +0530

    Update Longest K unique characters substring ( GFG ).cpp

--- Sliding Window/Variable Sized Window/Longest K unique characters substring ( GFG ).cpp
+++ Sliding Window/Variable Sized Window/Longest K unique characters substring ( GFG ).cpp
@@ -9,49 +9,41 @@ class Solution {
 public:
     int longestKSubstr(string s, int k) {
 
-        // Checking if k is more than the no. of unique characters in string itself
-        // if yes, then return -1 as no such substring is possible
-        set<char> st;
         int n = s.length();
+        set<char> st;
 
-        for (int i = 0; i < n; i++)
-            st.insert(s[i]);
+        for (auto c : s)
+            st.insert(c);
 
+        // Checking if k is more than the no. of unique characters in string itself
         // if yes, then return -1 as no such substring is possible
         if (k > st.size())
             return -1;
 
-
-        // Variable Size Sliding Window starts here
+        // VARIABLE SIZED SLIDING WINDOW starts here
         int i = 0, j = 0, ans = -1;
 
         // map to store the count of characters in the current window
         // and map.size() gives the number of unique characters
         unordered_map<char, int> mp;
 
-
-        // j++ will always be there so can be written at the end of while loop as well
-        // instead of writing j++ in the three different cases
         while (j < n) {
 
-            mp[s[j]]++;
+            mp[s[j]]++;                    // adding calculations of j
 
             // when there is less than k unique characters in the window, simply carry on
-            if (mp.size() < k)
-                j++;
+            if (mp.size() < k)    
+                continue;
 
-            // when there is k unique characters in the window, then it can be a candidate
-            // so updating the ans
-            else if (mp.size() == k) {
+            // when there is k unique characters in the window, then it can be a candidate, so updating the ans
+            else if (mp.size() == k)             // if condition is met
                 ans = max(ans, j - i + 1);
-                j++;
-            }
 
             // when there is more than k unique characters in the window, then start removing the
             // elements from the window
             else {
 
-                while (mp.size() > k) {
+                while (mp.size() > k) {            // removing calculations of i
                     mp[s[i]]--;
 
                     if (mp[s[i]] == 0)
@@ -59,10 +51,9 @@ public:
 
                     i++;
                 }
-
-                j++;
             }
-
+            
+            j++;
         }
 
         return ans;
