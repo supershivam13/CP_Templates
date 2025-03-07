0057682242a6ddc13f555fabcb4dbc5e782a15b7
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-07 14:51:53 +0530

    Update Kadane's Algo - Maximum Subarray Sum.cpp

--- Arrays/Kadane's Algo - Maximum Subarray Sum.cpp
+++ Arrays/Kadane's Algo - Maximum Subarray Sum.cpp
@@ -1,3 +1,27 @@
+//IMPORTANT
+// Kadane's Algorithm can also be used to find the MINIMUM SUBARRAY SUM (by 3 minor changes).
+
+ //        int ans = INT_MAX;    // should be initialised with INT_MAX       (CHANGE 1)
+        
+ //        int cur_sum = 0;     //initialise current sum as zero
+        
+ //        for (int i=0;i<arr.size();i++){
+            
+ //            cur_sum += arr[i];
+      
+ //            ans = min(ans,cur_sum);                                        (CHANGE 2)
+            
+ //            //If current sum becomes positive at any time, that means we can't include that
+ //            //sub-array in our solution, so we  re-initialise it to 0.
+ //            // THIS SHOULD BE DONE IN THE LAST, so we get atleast one answer for minimum subarray sum (Case when all numbers in array are positive)
+ //            if(cur_sum>0)                                                  (CHANGE 3)
+ //                cur_sum = 0;
+ //        }
+        
+ //        return ans;
+
+// -----------------------------------------------------------------------------------------------------------------------//
+
 // KADANE'S ALGORITHM
 // Time Complexity - O(N)
 // Space Complexity - O(1)
@@ -28,24 +52,3 @@ class Solution {
 };
 
 
-//IMPORTANT
-// Kadane's Algorithm can also be used to find the Minimum Subarray Sum (by 3 minor changes).
-
- //        int ans = INT_MAX;    // should be initialised with INT_MAX       (CHANGE 1)
-        
- //        int cur_sum = 0;     //initialise current sum as zero
-        
- //        for (int i=0;i<arr.size();i++){
-            
- //            cur_sum += arr[i];
-      
- //            ans = min(ans,cur_sum);                                        (CHANGE 2)
-            
- //            //If current sum becomes positive at any time, that means we can't include that
- //            //sub-array in our solution, so we  re-initialise it to 0.
- //            // THIS SHOULD BE DONE IN THE LAST, so we get atleast one answer for minimum subarray sum (Case when all numbers in array are positive)
- //            if(cur_sum>0)                                                  (CHANGE 3)
- //                cur_sum = 0;
- //        }
-        
- //        return ans;
