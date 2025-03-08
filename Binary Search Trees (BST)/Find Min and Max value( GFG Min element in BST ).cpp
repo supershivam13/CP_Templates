0e0aa5d833107cab0afd3a75bd8f99e520e22381
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-08 23:38:13 +0530

    Update Find Min and Max value( GFG Min element in BST ).cpp

--- Binary Search Trees (BST)/Find Min and Max value( GFG Min element in BST ).cpp
+++ Binary Search Trees (BST)/Find Min and Max value( GFG Min element in BST ).cpp
@@ -1,36 +1,33 @@
 // Minimum Value in a BST
-
 int minValue(Node* root) {
 
-    if (root == NULL)
+    if (root == NULL)        // 0 nodes are present
         return -1;
 
-    if (root->left == NULL)
+    if (root->left == NULL)    // 1 node is root, and left subtree is not present
         return root->data;
 
     Node* temp = root;
 
-    while (temp->left != NULL)
+    while (temp->left != NULL)   // going to the leftmost leaf node
         temp = temp->left;
 
     return temp->data;
 }
-
-// ***********************************************************************
+// *********************************************************************** //
 
 // Maximum Value in a BST ( NOT IN QUESTION )
-
 int maxValue(Node* root) {
 
-    if (root == NULL)
+    if (root == NULL)  // 0 nodes are present
         return -1;
 
-    if (root->right == NULL)
+    if (root->right == NULL)   // 1 node is root, and right subtree is not present
         return root->data;
 
     Node* temp = root;
 
-    while (temp->right != NULL)
+    while (temp->right != NULL)    // going to the rightmost leaf node
         temp = temp->right;
 
     return temp->data;
