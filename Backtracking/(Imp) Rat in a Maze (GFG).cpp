b2ad0753578c2c665b58384b58916c6e3348e3b0
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-08 13:49:45 +0530

    Update (Imp) Rat in a Maze (GFG).cpp

--- Backtracking/(Imp) Rat in a Maze (GFG).cpp
+++ Backtracking/(Imp) Rat in a Maze (GFG).cpp
@@ -1,4 +1,4 @@
-// In matrix or 2D Grid, x-axis is vertcal and y-axis is horizontal (opposite as of Coordinate Geometry)
+//In matrix or 2D Grid, x-axis is vertcal and y-axis is horizontal (opposite as of Coordinate Geometry)
 // Up , Right, Down , Left
 int dx[] = { -1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
@@ -8,15 +8,16 @@ public:
     string dir = "URDL";
     vector<vector<int>> grid;
     vector<string> ans;
-    int vis[6][6];
+    string output="";
+    int vis[6][6];      //maximum value of n is given as 5
 
-    bool isvalid(int x, int y, int n) {
+     // Funtion to check the valid moves only
+    bool isvalid(int x, int y, int n) {           
 
-        if (x < 0 or y < 0 or x > n - 1 or y > n - 1)
+        if (x < 0 or y < 0 or x > n - 1 or y > n - 1)            // checking it it is moving out of grid
             return false;
 
-        // if it is already visited or it has obstacle
-        if (vis[x][y] == 1 or grid[x][y] == 0)
+        if (vis[x][y] == 1 or grid[x][y] == 0)                // if it is already visited or it has obstacle
             return false;
 
         return true;
@@ -30,7 +31,7 @@ public:
             return;
         }
 
-        // If it has obstacle
+        // If it has obstacle { at the start itself (0,0) }
         if (grid[x][y] == 0)
             return;
 
@@ -39,12 +40,12 @@ public:
         for (int i = 0; i < 4; i++) {
             if (isvalid(x + dx[i], y + dy[i], n)) {
 
-                output += dir[i];
+                output += dir[i];                            // Pre-rocessing for next move
                 vis[x + dx[i]][y + dy[i]] = 1;
 
-                dfs2d(x + dx[i], y + dy[i], n, output);
+                dfs2d(x + dx[i], y + dy[i], n, output);      // next move
 
-                output.pop_back();
+                output.pop_back();                           // backtrack
                 vis[x + dx[i]][y + dy[i]] = 0;
             }
         }
@@ -53,16 +54,17 @@ public:
         vis[x][y] = 0;
     }
 
-    // Solution starts here
-    vector<string> findPath(vector<vector<int>> &mat, int n) {
-
+    // SOLUTION starts here
+    vector<string> findPath(vector<vector<int>> &mat) {
+        
+        int n = mat.size();
         grid = mat;
         int start = 0, end = 0;
 
         memset(vis, 0, sizeof(vis));
-        string output = "";
 
         dfs2d(0, 0, n, output);
+        
         sort(ans.begin(), ans.end());
 
         return ans;
