93fba3d8539bea94b11ad456a7155cdf3e5b4edf
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-08 20:36:07 +0530

    Update Valid Parentheses ( LeetCode ).cpp

--- Stacks/Valid Parentheses ( LeetCode ).cpp
+++ Stacks/Valid Parentheses ( LeetCode ).cpp
@@ -7,22 +7,22 @@ public:
 
         for (int i = 0; i < n; i++) {
 
-            if (x[i] == '(' or x[i] == '{' or x[i] == '[')
+            if (x[i] == '(' or x[i] == '{' or x[i] == '[') // any open bracket, push into stack
                 s.push(x[i]);
 
-            else if (x[i] == ')') {
+            else if (x[i] == ')') {        // check if matching open bracket is at top
                 if (s.empty() or s.top() != '(')
                     return false;
                 else
                     s.pop();
             }
-            else if (x[i] == '}') {
+            else if (x[i] == '}') {         // check if matching open bracket is at top
                 if (s.empty() or s.top() != '{')
                     return false;
                 else
                     s.pop();
             }
-            else if (x[i] == ']') {
+            else if (x[i] == ']') {         // check if matching open bracket is at top
                 if (s.empty() or s.top() != '[')
                     return false;
                 else
@@ -30,7 +30,7 @@ public:
             }
         }
 
-        if (s.size() == 0)
+        if (s.size() == 0)    // if stack is empty, only then parantheses are valid
             return true;
         else
             return false;
