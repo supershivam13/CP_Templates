a6fa4a0e2c18db683f5745555116612c22fa9922
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-09 18:10:42 +0530

    Update Bipartite Test in Graphs ( LeetCode Possible Bipartition ).cpp

--- Graphs/Bipartite Test in Graphs ( LeetCode Possible Bipartition ).cpp
+++ Graphs/Bipartite Test in Graphs ( LeetCode Possible Bipartition ).cpp
@@ -4,14 +4,14 @@ public:
     bool isBipartite(vector<vector<int>>&adj, vector<int>& visited, vector<int>& node_color, int node, int color){
 
         visited[node] = 1;
-        node_color[node] = color;
+        node_color[node] = color;        // marking the color of node
 
         for(auto v : adj[node]){
-            if(visited[v]==0){
-                if(isBipartite(adj, visited, node_color, v, color^1) == false)
+            if(visited[v]==0){            // check only if 'v' is unvisited
+                if(isBipartite(adj, visited, node_color, v, color^1) == false)      // if Recursive call returned false, then return false
                     return false;
             }
-            else if(node_color[node] == node_color[v])
+            else if(node_color[node] == node_color[v])            // if color of child and parent is same, then not possible to bi-partite graph
                 return false;
         }
 
@@ -21,18 +21,18 @@ public:
     //SOLUTION starts here
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
 
-        vector<vector<int>> adj(n+1);   // using 1-based index
-        vector<int> visited(n+1,0);
-        vector<int> node_color(n+1,-1);
+        vector<vector<int>> adj(n+1);    // using 1-based index
+        vector<int> visited(n+1,0);      // visited array
+        vector<int> node_color(n+1,-1);  // color array to keep track of colors of node (either 0 or 1) {initialised with -1}
 
-        for(auto dislike_pair : dislikes){
+        for(auto dislike_pair : dislikes){        // generating adjacency list, ( bi-directional edges )
             adj[dislike_pair[0]].push_back(dislike_pair[1]);
             adj[dislike_pair[1]].push_back(dislike_pair[0]);
         }
 
         // Checking for all the nodes in given graph
         for(int i=1;i<=n;i++){
-            if(visited[i]==0){
+            if(visited[i]==0){                                                // check only if unvisited
                 if(isBipartite(adj, visited, node_color, i, 0) == false)
                     return false;
             }
