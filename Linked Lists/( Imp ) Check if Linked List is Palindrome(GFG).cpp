2527a67fd7e09c4c0863ad54661720c1e488f541
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-09 22:28:27 +0530

    Update ( Imp ) Check if Linked List is Palindrome(GFG).cpp

--- Linked Lists/( Imp ) Check if Linked List is Palindrome(GFG).cpp
+++ Linked Lists/( Imp ) Check if Linked List is Palindrome(GFG).cpp
@@ -1,10 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution {
 public:
     // Function to return the length of a Linked List
-    int length(Node* head) {
+    int length(ListNode* head) {
 
         int count = 0;
-        Node* temp = head;
+        ListNode* temp = head;
 
         while (temp != NULL) {
             count++;
@@ -15,12 +26,12 @@ public:
 
     // Iterative method Function to reverse a Linked List ( Time - O(N), Space - O(1) )
     // Returns the head(first node) of the Reversed Linked List
-    Node* reverseList(struct Node *head)
+    ListNode* reverseList(struct ListNode *head)
     {
         // { Prev Curr Temp }
-        Node* prev = NULL;
-        Node* curr = head;
-        Node* temp;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* temp;
 
         while (curr != NULL) {
 
@@ -40,34 +51,29 @@ public:
 
 
     // SOLUTION STARTS HERE
-    bool isPalindrome(Node *head)
+    bool isPalindrome(ListNode *head)
     {
         // List of one node is always a palindrome
         if (head->next == NULL)
             return true;
 
-        Node* slow = head;
-        Node* fast = head->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
         while (fast != NULL and fast->next != NULL) {
             fast = fast->next->next;
             slow = slow->next;
         }
-
-        int l = length(head);
-
-        // if length is even, then there will be two middle nodes
-        // and we have to choose the second middle node
-        if (l % 2 == 0)
-            slow = slow->next;
+        // After this loop, 'slow' points to the MIDDLE of the linked list
+        // in case of even length linked list, there will be two middle nodes, slow points to second middle node
 
         // Reversing the Linked List from the middle point
-        Node* end = reverseList(slow);
-        Node* start = head;
+        ListNode* end = reverseList(slow);      // second of the Linked list is reversed
+        ListNode* start = head;
 
-        while (end != NULL) {
+        while (end != NULL) {   // simply traversing from 'start' and 'end' and checking if data is equal or not
 
-            if (end->data != start->data)
+            if (end->val != start->val)
                 return false;
 
             start = start->next;
