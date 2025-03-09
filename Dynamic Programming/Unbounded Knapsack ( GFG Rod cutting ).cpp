5907f85b85003b4cb26d82555bba48f85f877ddd
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-03-10 02:26:04 +0530

    Update Unbounded Knapsack ( GFG Rod cutting ).cpp

--- Dynamic Programming/Unbounded Knapsack ( GFG Rod cutting ).cpp
+++ Dynamic Programming/Unbounded Knapsack ( GFG Rod cutting ).cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
 
-    int cutRod(int price[], int n) {
-
+    int cutRod(vector<int> price) {
+        
+        int n = price.size();
+        
         // Array which contains the permissible length of the pieces
         // (In this question, all lengths from 1 to N are permissible )
         // (Otherwise, this array will also be given as input )
@@ -19,15 +21,16 @@ public:
                 if (i == 0 or j == 0)
                     dp[i][j] = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= n; i++) {          // i --> [1, N]
+            for (int j = 1; j <= n; j++) {      // j --> [1,M]
 
                 // if permssible length of piece is greater than the rod itself
                 if (a[i - 1] > j)
-                    dp[i][j] = dp[i - 1][j];
-                // else max of (included, not included)
-                else
-                    dp[i][j] = max(price[i - 1] + dp[i][j - a[i - 1]], dp[i - 1][j]);
+                    dp[i][j] = dp[i - 1][j];        // excluded
+                    
+                else        // else max of (included, not included)
+                    dp[i][j] = max(price[i - 1] + dp[i][j - a[i - 1]], dp[i - 1][j]); 
+                    // when included, add the value as well, here value is price[i-1]
             }
         }
 
