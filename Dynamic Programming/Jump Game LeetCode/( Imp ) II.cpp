838c2d64786ea5fb597a2da518af1fc947b6d66f
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-05-27 20:21:24 +0530

    Update ( Imp ) II.cpp

--- Dynamic Programming/Jump Game LeetCode/( Imp ) II.cpp
+++ Dynamic Programming/Jump Game LeetCode/( Imp ) II.cpp
@@ -4,33 +4,28 @@
 
 class Solution {
 public:
-  int jump(vector<int> a) {
-    
-    int n=a.size();
-    if (n == 1)
-      return 0;
-    
-    int maxReach = a[0];
-    int step=a[0];
-    int jumps=1;
-    
-    for(int i=1;i<n;i++){
-        
-        if(i==n-1)
-            return jumps;
-        else{
-            maxReach = max(maxReach,a[i]+i);
-            step--;
-            
-            if(step==0){
-                jumps++;
-                step=maxReach - i;
+    int jump(vector<int>& nums) {
+
+        int n = nums.size();
+        int jumps = 0;      // final minimum jumps needed
+        int l = 0;          // think of reachable ranges as [l,r] 
+        int r  = 0;         // l = left, r = right, initially [l,r] range is [0,0]
+
+        while(r < n-1 ){    // if r reaches the end of array or passes it, break the while loop
+
+            int maxIndex = 0;       // maximum index we can reach,
+
+            for(int i=l;i<=r;i++){      // taking the maxIndex we can reach for each [l,r] range
+                maxIndex = max(maxIndex, i + nums[i]);
             }
+
+            l = r + 1;      // l will always be one more to previous range high
+            r = maxIndex;   // r will become the maxIndex which can be reached in next range
+            jumps++;        // we took one more jump
         }
+
+        return jumps;
     }
-    
-    return jumps;
-  }
 };
 
 
