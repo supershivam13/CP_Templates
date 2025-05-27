791f22e390fe7dc414713ed78d87d2589e9d0a36
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-05-27 20:27:38 +0530

    Update ( Imp ) II.cpp

--- Dynamic Programming/Jump Game LeetCode/( Imp ) II.cpp
+++ Dynamic Programming/Jump Game LeetCode/( Imp ) II.cpp
@@ -13,12 +13,14 @@ public:
 
         while(r < n-1 ){    // if r reaches the end of array or passes it, break the while loop
 
-            int maxIndex = 0;       // maximum index we can reach,
+            int maxIndex = 0;       // maximum index we can reach
 
+            // Check all positions from l to r
             for(int i=l;i<=r;i++){      // taking the maxIndex we can reach for each [l,r] range
                 maxIndex = max(maxIndex, i + nums[i]);
             }
 
+            // Update bounds for the next jump
             l = r + 1;      // l will always be one more to previous range high
             r = maxIndex;   // r will become the maxIndex which can be reached in next range
             jumps++;        // we took one more jump
