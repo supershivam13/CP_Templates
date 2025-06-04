7e6cd5d096d76465d7bfa14c140a4fc5d4f7cc5a
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-06-04 15:28:22 +0530

    Update ( GFG Transform to Sum Tree ).cpp

--- Binary Trees/( GFG Transform to Sum Tree ).cpp
+++ Binary Trees/( GFG Transform to Sum Tree ).cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-
-    int changetoSum(Node *root) {
-        if (root == NULL)    // Base Conditon
+        int changetoSum(Node* node) {
+        if (node == NULL)    // Base Conditon
             return 0;
 
-        // Case of leaf node {changing it's data to 0 and returning the original data stored in it}
-        if (root->left == NULL && root->right == NULL) {
+        // Case of leaf root {changing it's data to 0 and returning the original data stored in it}
+        if (node->left == NULL && node->right == NULL) {
             int original = node->data;
-            node->data = 0;  // Leaf node becomes 0
+            node->data = 0;  // Leaf root becomes 0
             return original;
         }
 
-        int leftSum = SumTree(node->left);
-        int rightSum = SumTree(node->right);
+        int leftSum = changetoSum(node->left);
+        int rightSum = changetoSum(node->right);
 
         int original = node->data;
-        node->data = leftSum + rightSum;  // Update node to sum of its left and right subtree
+        node->data = leftSum + rightSum;  // Update root to sum of its left and right subtree
 
-        return original + leftSum + rightSum;  // Return total sum including this node
+        return original + leftSum + rightSum;  // Return total sum including this root
     }
 
     // CODE starts here
     void toSumTree(Node *node)
     {
-        int sum = SumTree(node);
+        int sum = changetoSum(node);
     }
 };
