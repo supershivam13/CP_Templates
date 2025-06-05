79295aeff71bee03483ad0a75e6e80ea5a89b3b8
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-06-05 20:00:53 +0530

    Update Next Greater Element ( GFG NGE ).cpp

--- Stacks/Next Greater Element ( GFG NGE ).cpp
+++ Stacks/Next Greater Element ( GFG NGE ).cpp
@@ -16,42 +16,41 @@
 
 // **********************************************************************************************
 
-class Solution
-{
+class Solution{
 public:
-    vector<long long> nextLargerElement(vector<long long> a, int n) {
-        stack<long long> s;
-        vector<long long> v;
+    vector<int> nextLargerElement(vector<int> arr) {
+        
+        int n = arr.size();
+        stack<int> s;
+        vector<int> ans;
 
         for (int i = n - 1; i >= 0; i--) {    // traversing the array from end to start
 
             // if no elements in stack, then ans is -1  (given in question, if no NGE, then -1)
             if (s.size() == 0)
-                v.push_back(-1);
+                ans.push_back(-1);
 
             // if size>0 and top element is greater than a[i], then s.top() is answer
-            else if (s.size() > 0 and s.top() > a[i])
-                v.push_back(s.top());
+            else if (s.size() > 0 and s.top() > arr[i])
+                ans.push_back(s.top());
 
             // if size>0 and top element is smaller or equal to a[i]
-            else if (s.size() > 0 and s.top() <= a[i]) {
+            else if (s.size() > 0 and s.top() <= arr[i]) {
 
                 // pop elements until stack is empty or top element becomes greater than a[i]
-                while (s.size() > 0 and s.top() <= a[i])
+                while (s.size() > 0 and s.top() <= arr[i])
                     s.pop();
 
                 if (s.size() == 0)
-                    v.push_back(-1);
+                    ans.push_back(-1);
                 else
-                    v.push_back(s.top());
+                    ans.push_back(s.top());
             }
             
-            s.push(a[i]);   // inserting the element in the stack after its processing for NGE is done
+            s.push(arr[i]);   // inserting the element in the stack after its processing for NGE is done
         }
 
-        // reversing the result vector
-        reverse(v.begin(), v.end());
-
-        return v;
+        reverse(ans.begin(), ans.end());    // reversing the result vector
+        return ans;
     }
 };
