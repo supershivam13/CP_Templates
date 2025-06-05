72fafe166235264c39a3f310958558d676990339
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-06-05 18:38:04 +0530

    Update (Imp) ( LeetCode Word Search ).cpp

--- Backtracking/(Imp) ( LeetCode Word Search ).cpp
+++ Backtracking/(Imp) ( LeetCode Word Search ).cpp
@@ -1,42 +1,48 @@
 class Solution {
+    // Directions: Up, Right, Down, Left
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = { 0, 1, 0, -1};
+
 public:
-
-    //pass board by reference otherwise it'll result in TLE
-    bool DFS(vector<vector<char>>& board, string word, int i, int j, int n) {
-
-        //check if all the alphabets in the word is checked (i.e we reached the end of the word)
-        if (n == word.size())
+    bool solve(vector<vector<char>>& board, int row, int col, int i, int j, string& word, int index, int n) {
+        // Base case: entire word has been matched
+        if (index == n)
             return true;
 
-        //check if i and j are out of bound or if the characters aren't equal
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n])
+        // Boundary check, Visited check and Mismatch check
+        if (i < 0 || j < 0 || i >= row || j >= col || board[i][j] == '.' || board[i][j] != word[index])
             return false;
 
-        //mark as visited, so that this character can't be used twice
-        // Example - if word = 'ABA' and we have A and B adjacent, then from B, dfs call to left will take
-        // it as true (i.e we found the word but in real, it is not there )
-        board[i][j] = '0';
+        char temp = board[i][j];
+        board[i][j] = '.';         // Mark current cell as visited
 
-        //branch out in all 4 directions
-        bool status = DFS(board, word, i + 1, j, n + 1) ||  //down
-                      DFS(board, word, i, j + 1, n + 1) ||  //right
-                      DFS(board, word, i - 1, j, n + 1) ||  //up
-                      DFS(board, word, i, j - 1, n + 1);    //left
+        // Explore all 4 directions
+        for (int d = 0; d < 4; d++) {
+            int x = i + dx[d], y = j + dy[d];
+            if (solve(board, row, col, x, y, word, index + 1, n))
+                return true;
+        }
 
-        //change the character back for other searches
-        board[i][j] = word[n];
+        board[i][j] = temp;     // Backtrack: restore the original value
 
-        return status;
+        return false;
     }
 
-    // Solution starts here
+    // CODE starts here
     bool exist(vector<vector<char>>& board, string word) {
+        int row = board.size();
+        int col = board[0].size();
+        int n = word.size();
 
-        for (int i = 0; i < board.size(); i++)
-            for (int j = 0; j < board[i].size(); j++)
-                //check if the first characters are equal, only then call DFS
-                if (board[i][j] == word[0] && DFS(board, word, i, j, 0))
-                    return true;
+        // Try to start DFS from every cell that matches the first character
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == word[0]) {
+                    if (solve(board, row, col, i, j, word, 0, n))
+                        return true;
+                }
+            }
+        }
 
         return false;
     }
