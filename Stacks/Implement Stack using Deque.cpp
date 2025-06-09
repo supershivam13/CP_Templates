2d213f9427629909fa4e7ecbb8c756563d362dc8
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2025-06-09 20:01:18 +0530

    Update Implement Stack using Deque.cpp

--- Stacks/Implement Stack using Deque.cpp
+++ Stacks/Implement Stack using Deque.cpp
@@ -1,52 +1,29 @@
 class MyStack {
 public:
-
     deque<int>q;
-    MyStack() {
+    MyStack() {}
 
-    }
-
-    // removing (q.size()-1) elements from the front and inserting them from the back
-    // so that stack property of LIFO is maintained
     void push(int x) {
         q.push_back(x);
 
-        int sz = q.size() - 1;
-        while (sz--) {
+        int times = q.size() - 1;          // removing (q.size()-1) elements from the front and inserting them from the back,
+        while (times--) {                  // so that stack property of LIFO is maintained
             q.push_back(q.front());
             q.pop_front();
         }
     }
 
-    // Removes the element on top of the stack and returns that element.
     int pop() {
-
-        int x = -1;
-        if (!q.empty())
-        {
-            x = q.front();
-            q.pop_front();
-        }
+        int x = q.front();
+        q.pop_front();
         return x;
     }
 
     int top() {
-        if (!q.empty())
-            return q.front();
-        else
-            return -1;
+        return q.front();
     }
 
     bool empty() {
-        return q.empty() == true ? true : false;
+        return q.empty();
     }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
