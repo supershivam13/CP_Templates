8eb1620c3f87689f00cb17a224dfa9b81e5136f8
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-03 00:35:18 +0530

    Update Segment Tree ( SPOJ Range Minimum Query ).cpp

--- Segment Tree/Segment Tree ( SPOJ Range Minimum Query ).cpp
+++ Segment Tree/Segment Tree ( SPOJ Range Minimum Query ).cpp
@@ -1,197 +1,71 @@
-/*
-    Author      : SHIVAM SAHU
-    Institution : IIT PATNA
-    Country     : INDIA
-*/
+// These 3 parameters are always there in any Segment Tree function
+// Segment Index (idx)
+// Segment Start (start)
+// Segment End (end)
 
-#include<bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp>
-// using namespace __gnu_pbds;
+// If using 0-based indexing for the segment tree array:
+// Current node index = i
+// Left child  = 2*i + 1
+// Right child = 2*i + 2
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
-// PRAGMA reduces execution time a little
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("O3")
-#pragma GCC target ("avx")
+int a[1000006];                // lim = 10*6, array for taking input
+int segTree[4 * 1000006];      // size should be 4*n, so making it of 4 * 10^6
 
-// The typedef is used to give data type a new name
-// #define in C is a directive which is used to #define alias(shortcuts)
-typedef long long ll;
-typedef long double ld;
-//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// Function to build the segment tree array before processing queries
+void build(int idx, int start, int end) {
 
-#define fio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-
-#define f(i,n)          for(int i=0;i<n;i++)
-#define F(i,a,n)        for(int i=a;i<n;i++)
-#define fo(i,a,b)       for(int i=a;i<=b;i++)
-#define arrin(a,n)      for(int i=0;i<n;i++)cin>>a[i]
-#define arrout(a,n)     for(int i=0;i<n;i++)cout<<a[i]<<" "
-#define print2d(a,n,m)  for(int i=0;i<n;i++){for(int j=0;j<m;j++)cout<<a[i][j]<<" ";cout<<"\n";}
-#define w(x)            int x; cin>>x; while(x--)
-
-#define ff              first
-#define ss              second
-#define int             long long
-#define double          long double
-#define eb              emplace_back
-#define pb              push_back
-#define pf              push_front
-#define popb            pop_back
-#define popf            pop_front
-#define mp              make_pair
-#define rt              return
-#define br              break
-#define ct              continue
-#define elif            else if
-
-#define dbg             cout<<"\nhuehue\n"
-#define sayy            cout<<"YES\n"
-#define sayn            cout<<"NO\n"
-#define debug(a)        cerr << #a << ": " << a << endl
-
-
-#define pii             pair<int,int>  // ( p.first , p.second )
-#define pss             pair<string,string>
-
-#define mii             map<int,int> // ( insert(key,value), begin(), end(), size(), empty() )
-#define mci             map<char, int>
-#define msi             map<string, int>
-#define mss             map<string, string>
-#define umii            unordered_map<int,int>
-// Itertating over a map m ==>> for( auto i : m ){ cout<<i.first<<" "<<i.second<<endl; }
-
-#define vi              vector<int>  // ( size(), resize(), begin(), end(), empty() )
-#define vb              vector<bool>
-#define vii             vector<pair<int,int>>
-#define vvi             vector<vector<int>>
-
-#define si              set<int>  // stores in ascending order // ( insert(), size(), empty(), begin(), end() )
-#define sid             set<int, greater<int> >  // stores in descending order
-#define sii             set<pair<int,int>>
-
-#define li              list <int> // ( pb(), pf(), popb(), popf(), reverse(), sort(), empty(), size(),
-//  remove(), front(), back() insert() )
-
-#define maxheap         priority_queue<int>  // by default, max heap ( push(), pop(), top(), empty(), size() )
-#define minheap         priority_queue<int,vi,greater<int>>  // min heap ( push(), pop(), top(), empty(), size() )
-
-
-#define setbits(x)      __builtin_popcountll(x) // return the count of set bits
-#define startzeroes(x)  __builtin_clzll(x) //return the count the leading zeros of the integer(clz=count leading zero’s)
-#define endzeroes(x)    __builtin_ctzll(x) // used to count the trailing zeros of the given integer(ctz = count trailing zeros)
-
-#define ps(x,y)         fixed<<setprecision(y)<<x  // (always prefer double) works with float and double..ps(x,y)=x with y digits after decimal
-#define dma(a,n,type)   type *a=new type[n];  // dynamic memory allocation (Example : type*a = new int[n]; )
-
-const int inf   = numeric_limits<int>::max();
-const double pi = 3.1415926535898;
-const int mod   = 1000000007;
-const int lim   = 1000006;
-
-#define all(v)          v.begin(),v.end()
-#define sum(v)          accumulate(v.begin(),v.end(),(ll)0)
-#define asum(a,n)       accumulate(a,a+n,(ll)0)
-#define rev(v)          reverse(v.begin(),v.end());
-//  stoi() function - converts string to integer ( Example- int x = stoi(s) )
-//  stoll() funtion - converts string to long long ( Example- long long x = stoll(s) )
-//  to_string() function - converts integer to string ( Example- string s = to_string(x) )
-
-
-void c_p_c()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
-}
-
-// lim = 10*6, array for taking input
-int a[lim];
-
-// Array for storing the Segment Tree
-// size should be 4*n, so making it of 4 * 10^6
-int segtree[4 * lim];
-
-// These 3 parameters are always there in any Segment Tree function
-// s_ind = segment index
-// ss = segment start
-// se = segment end
-
-// qs = query start
-// qe = qeuery end
-
-
-// Function to make the segment tree array before processing queries
-void build(int s_ind, int ss, int se) {
-
-    // when segment has only 1 element
-    if (ss == se) {
-        segtree[s_ind] = a[ss];
+    if (start == end) {					 // Only one element (Leaf Node)
+        segTree[idx] = a[start];
         return;
     }
 
-    int mid = ss + ((se - ss) / 2);
+    int mid = start + ((end - start) / 2);
 
-    // left subtree
-    build(2 * s_ind, ss, mid);
-    // right subtree
-    build(2 * s_ind + 1, mid + 1, se);
+    build(2 * idx + 1, start, mid);
+    build(2 * idx + 2, mid + 1, end);
 
-    // getting the result from left subtree and right subtree
-    segtree[s_ind] = min(segtree[2 * s_ind], segtree[2 * s_ind + 1]);
-
+    segTree[idx] = min(segTree[2 * idx + 1], segTree[2 * idx + 2]);
 }
 
 
-// Function to answer a query in O(logN) time ( brute force takes O(N) to process a single query )
-int query(int s_ind, int ss, int se, int qs, int qe) {
+// Function to answer a query in O(logN) time ( Brute Force takes O(N) to process a single query )
+int query(int idx, int start, int end, int left, int right){
 
-    // completely outside
-    if (qs > se or qe < ss)
-        return inf;
+    if(left > end || right < start)         // if queried range is completely out of range[start, end]
+        return INT_MAX;
+     
+    if( start >= left && right >= end)      // if queried ranges lies completely in range[start, end]
+        return segTree[idx];
+    
+    int mid = start + ((end - start) / 2);
 
-    // completely inside ( segment completely inside the query range )
-    if (ss >= qs and se <= qe)
-        return segtree[s_ind];
+    int leftValue = query(2 * idx + 1, start, mid, left, right);
+    int rightValue = query(2 * idx + 2, mid + 1, end, left, right);
 
-    int mid = (ss + se) / 2;
-
-    // getting the result from the left subtree
-    int left_result = query(2 * s_ind, ss, mid, qs, qe);
-    // getting the result from the right subtree
-    int right_result = query((2 * s_ind) + 1, mid + 1, se, qs, qe);
-
-    return min(left_result, right_result);
+    return min(leftValue, rightValue);
 }
 
-int32_t main()
-{
-    fio;
+// CODE starts here
+int main(){
     int n;
     cin >> n;
+	
+    for(int i = 0; i<n; i++)
+    	cin >> a[i];
 
-    fo(i, 1, n)
-    cin >> a[i];
+    build(0, 0, n-1);
 
-    // Building the segment tree array
-    build(1, 1, n);
-
-    // number of queries
-    int q;
+    int q;						 // Number of queries  (return the minimum element in the range [L,R])
     cin >> q;
     while (q--) {
 
-        // Query is to give the minimum element in the range L to R
-        int qs, qe;
-        cin >> qs >> qe;
+        int left, right;
+        cin >> left >> right;
 
-        // Here, we are passing + 1 as question is using 0-based indexing
-        // and we have used 1-based indexing for making the segment tree
-        cout << query(1, 1, n, qs + 1, qe + 1) << endl;
+        cout << query(0, 0, n-1, left, right) << endl;
     }
 }
