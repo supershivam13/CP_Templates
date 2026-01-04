8213b1e0280ed0f857aa09233aa8bde0b45a700f
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-04 12:40:36 +0530

    Update Diameter of a Binary Tree - O(N^2) (GFG).cpp

--- Binary Trees/Diameter of a Binary Tree - O(N^2) (GFG).cpp
+++ Binary Trees/Diameter of a Binary Tree - O(N^2) (GFG).cpp
@@ -1,13 +1,9 @@
-// Time Complexity - O( N^2 )
-// BRUTE FORCE APPROACH
-
-
+// BRUTE FORCE APPROACH - O( N^2 )
 class Solution {
   public:
-  
     int height(Node* root){    // Normal Height of a Binary Tree function
         
-        if(root==NULL)
+        if(root == NULL)
             return 0;
             
         int left = height(root->left);
@@ -19,13 +15,13 @@ class Solution {
     // DIAMETER = Number of EDGES (not nodes) on the longest path between two leaf nodes
     int diameter(Node* root) {
         
-        if(root==NULL)
+        if(root == NULL)
             return 0;
         
         int d1 = height(root->left) + height(root->right);   // If asked as Diameter = Number of nodes, then add 1 to 'd1'.
         int d2 = diameter(root->left);        // when diameter lies in the left subtreee
         int d3 = diameter(root->right);       // when diameter lies in the right subtreee
         
-        return max( d1, max(d2,d3) );
+        return max(d1, max(d2, d3));
     }
 };
