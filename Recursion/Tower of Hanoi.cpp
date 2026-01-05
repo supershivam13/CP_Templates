31fa7a9d7271972c6be04547d814c5f37a4ffbb2
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-06 00:05:07 +0530

    Update Tower of Hanoi.cpp

--- Recursion/Tower of Hanoi.cpp
+++ Recursion/Tower of Hanoi.cpp
@@ -7,7 +7,6 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char helper_rod) {
         cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl;
         return;
     }
-
     // moving n - 1 rods from Source to Helper
     towerOfHanoi(n - 1, from_rod, helper_rod, to_rod);
 
@@ -17,7 +16,6 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char helper_rod) {
     // moving n - 1 rods from Helper to Destination
     towerOfHanoi(n - 1, helper_rod, to_rod, from_rod);
 }
-
 
 int main(){
     
