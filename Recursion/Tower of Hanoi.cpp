19b875a363a966e6c606f891f00106af0b8e54b3
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-06 00:04:24 +0530

    Update Tower of Hanoi.cpp

--- Recursion/Tower of Hanoi.cpp
+++ Recursion/Tower of Hanoi.cpp
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+void towerOfHanoi(int n, char from_rod, char to_rod, char helper_rod) {
 
-    // Base Condition
-    if (n == 1) {
+    if (n == 1) {        // Base Condition
         cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl;
         return;
     }
 
     // moving n - 1 rods from Source to Helper
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    towerOfHanoi(n - 1, from_rod, helper_rod, to_rod);
 
     // Moving n-th disk from Source to Destination
     cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
 
     // moving n - 1 rods from Helper to Destination
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    towerOfHanoi(n - 1, helper_rod, to_rod, from_rod);
 }
 
 
-int main()
-{
-    int n = 4; // Number of disks
-
-    // A = Source, C = Destination, B = Helper
-    towerOfHanoi(n, 'A', 'C', 'B');
-
+int main(){
+    
+    int n = 4;     // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B');   // A = Source, C = Destination, B = Helper
 }
