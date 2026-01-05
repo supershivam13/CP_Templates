d465e3f10c870d8da96805cfc9df443807a37158
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-06 02:03:10 +0530

    Update DFS on 2D Grid ( Leetcode Number of Islands).cpp

--- Graphs/DFS on 2D Grid ( Leetcode Number of Islands).cpp
+++ Graphs/DFS on 2D Grid ( Leetcode Number of Islands).cpp
@@ -4,27 +4,26 @@ public:
     int dy[4] = { 0,  1, 0, -1 };
 
     void dfs(int x, int y, int n, int m, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-        // Boundary Check, Visited Check and is land ('1')
-        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] != '1' || visited[x][y]==true)
+        // Boundary Check, Visited Check and is land Check ('1')
+        if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y] == true || grid[x][y] == '0')
             return;
 
         visited[x][y] = true;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++)
             dfs(x + dx[i], y + dy[i], n, m, grid, visited);
-        }
     }
+
     // CODE starts here
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         int islands = 0;
 
         vector<vector<bool>> visited(n, vector<bool>(m, false));
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && visited[i][j]==0) { // check if found land {grid[i][j]==1} and it is unvisited
+                if (grid[i][j] == '1' && visited[i][j] == 0) { // check if found land {grid[i][j] == 1} and it is unvisited
                     dfs(i, j, n, m, grid, visited);
                     islands++;
                 }
