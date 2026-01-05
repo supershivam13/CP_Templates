8ee6bff8db87564cdd91397b7b0bb39b2a8ee010
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-05 20:28:03 +0530

    Update Longest Common Subsequence - Bottom Up ( LeetCode LCS ).cpp

--- Dynamic Programming/Longest Common Subsequence - Bottom Up ( LeetCode LCS ).cpp
+++ Dynamic Programming/Longest Common Subsequence - Bottom Up ( LeetCode LCS ).cpp
@@ -8,27 +8,29 @@ class Solution {
 public:
     int longestCommonSubsequence(string x, string y) {
 
-        int m = x.length();
-        int n = y.length();
+        int n = x.length();
+        int m = y.length();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
         // Initialisation ( Base condition )
-        fo(i, 0, m) {
-            fo(j, 0, n) {
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
                 if (i == 0 or j == 0)
                     dp[i][j] = 0;
             }
         }
 
-        fo(i, 1, m) {
-            fo(j, 1, n) {
-                // if character of both strings matches
-                if (x[i - 1] == y[j - 1])
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                
+                if (x[i - 1] == y[j - 1])                // if character of both strings matches
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
                     dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
 
-        return dp[m][n];
+        return dp[n][m];
     }
 };
