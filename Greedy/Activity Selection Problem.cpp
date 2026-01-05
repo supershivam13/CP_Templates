a19154f1063581703b4fb66c3559cfe9118a4894
Shivam Sahu <54952935+supershivam13@users.noreply.github.com>
2026-01-05 20:14:00 +0530

    Update Activity Selection Problem.cpp

--- Greedy/Activity Selection Problem.cpp
+++ Greedy/Activity Selection Problem.cpp
@@ -1,6 +1,7 @@
+//(JO PEHLE KHATAM HO RHA H USKO LE LO),
 bool cmp(vector<int>& a, vector<int>& b){ // sorting the vector in ascending order of end time
-                                          //(JO PEHLE KHATAM HO RHA H USKO LE LO),
-    if(a[1]==b[1])          // if end times are equal, then in ascending order of start time
+                                          
+    if(a[1]==b[1])                      // if end times are equal, then sort in ascending order of start time
         return a[0]<b[0];
     
     return a[1] < b[1];
@@ -12,15 +13,16 @@ class Solution {
         
         int n = start.size();
         vector<vector<int>> pairs;
-        for(int i=0; i<n; i++)          // storing the values as pair [start,end] in vector 'pairs'
+      
+        for(int i = 0; i < n; i++)                  // storing the values as pair [start,end] in vector 'pairs'
             pairs.push_back({start[i], end[i]});
         
-        sort(pairs.begin(), pairs.end(), cmp); // sorting the 'pairs' using comparator function
+        sort(pairs.begin(), pairs.end(), cmp);       // sorting the 'pairs' using comparator function
         
-        int take = 1;               // we took the first job
+        int take = 1;                   // we took the first job
         int last = pairs[0][1];
         
-        for(int i=1; i<n; i++){
+        for(int i = 1; i < n; i++){
             if(last < pairs[i][0]){
                 last = pairs[i][1];
                 take++;
